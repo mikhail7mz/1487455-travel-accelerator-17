2c6fa5410a4b4f4d13ad853af28c549e85a94a3f
Mikhail <65595208+mikhail7mz@users.noreply.github.com>
2024-04-28 20:21:49 +0300

    Стилизует блок training.

--- source/js/main.js
+++ source/js/main.js
@@ -3,9 +3,11 @@ import {initFixedHeader} from './fixed-header';
 import {initMenu} from './menu';
 import {initPromoSlider} from './promo-slider';
 import {initToursSlider} from './tours-slider';
+import {initTrainingSlider} from './training-slider';
 
 detectWebp();
 initFixedHeader();
 initMenu();
 initPromoSlider();
 initToursSlider();
+initTrainingSlider();
---
+++ source/js/training-slider.js
@@ -0,0 +1,30 @@
+import { initSlider } from './slider';
+import { Navigation } from 'swiper/modules';
+
+const parameters = {
+  spaceBetween: 15,
+  initialSlide: 2,
+  modules: [ Navigation ],
+  navigation: {
+    nextEl: '[data-next]',
+    prevEl: '[data-prev]',
+    disabledClass: 'disabled',
+  },
+  breakpoints: {
+    768: {
+      spaceBetween: 20,
+      initialSlide: 0,
+      slidesPerView: 3,
+    },
+    1440: {
+      spaceBetween: 20,
+      initialSlide: 0,
+      slidesPerView: 4,
+      allowTouchMove: false,
+    }
+  },
+};
+
+const initTrainingSlider = () => initSlider('[data-training-slider]', parameters);
+
+export {initTrainingSlider};
